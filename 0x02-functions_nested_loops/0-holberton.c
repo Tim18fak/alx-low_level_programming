8bdbffe18696eb80448c663d331240b14638fb01
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-14 13:05:19 -0800

    0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description: prints 'Holberton \n'
+ * Return: Always 0 (success0
+ */
+
+int  main(void)
+
+{
+	char str[] = 'Holberton\n";
+
+	int i = 0;
+
+	while (stri[i] != '\0')
+
+	{
+
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <helloworldd>
+
+/**
+ * print_alphabet -Entry point
+ * Description: a function that prints the alphabet'
+ * in lowercase followed by a new line
+ * Return; void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
