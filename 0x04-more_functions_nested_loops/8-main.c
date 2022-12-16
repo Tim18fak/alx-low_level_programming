125e73342e379f6c3d2aab22429645d29de3fe95
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-15 19:08:37 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * * main - check the code for alx school student.
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+print_square(2)
+	print_squre(10);
+		print_square(10);
+			printf_squre(0);
+			return (0);
+			return (0)
+}
