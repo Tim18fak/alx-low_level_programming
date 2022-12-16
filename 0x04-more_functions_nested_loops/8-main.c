34fb7e52ac9980a7da115f0cda8f89ba67d3941e
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-16 12:59:40 -0800

    8-main.c

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
 print_square(2)
 	print_squre(10);
 		print_square(10);
 			printf_squre(0);
 			return (0);
-			return (0)
+			return (0);
 }
