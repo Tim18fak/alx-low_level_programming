3d4a8ccb1f5ab390b939f96514340675d271f4b9
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-15 18:20:37 -0800

    0x03-debugging

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,24 @@
  */
 void more_numbers(void)
 {
-int number1, number2;
+char n, c;
+int i = 0;
 
-for (number1 = 0; number1 < 10; number1++)
+while (i < 10)
 {
-for (number2 = 0; number2 <= 14; number2++)
+for (n = 0; n <= 14; n++)
+{
+c = n;
+if (n > 9)
+{
+_putchar('l');
 
-{
-if (number2 > 9)
-{
-_putchar((number2 / 10) + '0');
+c = n % 10;
 }
-_putchar((number2 % 10) + '0');
+_putchar('0' + c);
 }
-_putchar(10);
+_putchar('\n');
+i++;
 }
 }
 
