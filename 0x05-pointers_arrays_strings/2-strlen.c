bdf65729708b1db7b9a96da347a1a2984266442b
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-19 06:27:47 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * swap_int - swap the values of two integers
- * @a: integer to swap
- * @b: integer to swap
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-void swap_int(int *a, int *b)
+int _strlen(char *s)
 {
-	int m;
+	int longi = 0;
 
-	m = *a;
-	*a = *b;
-	*b = m;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
