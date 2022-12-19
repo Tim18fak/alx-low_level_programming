d20eac4371f654081154ba16e6b84db65cbb319f
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-19 06:42:28 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/**
+ * _puts - check the code
+ * @str: string to print
+ * Return: Always 0.
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str++);
+	}
+		_putchar('\n')
+}
