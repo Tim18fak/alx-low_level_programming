a9ba3eda7e01be2024c697ca51587225968c0a2f
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-19 15:57:59 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,49 +1,32 @@
-#include "main.h"
-
-
-
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
  *
- * Return: the int converted from the string
- */
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
 
-int _atoi(char *s)
+int main(void)
+
 {
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;	
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')															++d;
-																	if (s[i] >= '0' && s[i] <= '9')
-																	{
-		digit = s[i] - '0';
-		if (d % 2)
-			digit = -digit;	
-		n = n * 10 + digit;
-		f = 1;
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;
-		f = 0;
-																	}
-																	i++;
-	}
-	
-	if (f == 0)
-		return (0);
-
-	return (n);
+    int nb;
+    nb = _atoi("98");
+    printf("%d\n", nb);
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("          ------++++++-----+++++--98");				    printf("%d\n", nb);
+    nb = _atoi("2i4748364");
+    printf("%d\n", nb);
+    nb = _atoi("0");
+    printf("%d\n", nb);
+    nb = _atoi("Suite 402");							    printf("%d\n", nb);
+    nb = _atoi("         +      +    -     98 Battery Street; San Francisco, C )
+    printf("%d\n", nb);
+    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", nb);
+    return (0);
 }
 
 
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,51 +1,23 @@
-nt main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - generates random password for 101-crackme
+ * Return: zero
+ */
+int main(void)
 {
+	int sum;
+	char c;
 
-		int pass[100];
-
-			int i, sum, n;
-
-
-
-				sum = 0;	
-
-
-
-					srand(time(NULL));
-
-
-
-						for (i = 0; i < 100; i++)
-
-								{
-
-											pass[i] = rand() % 78;
-
-													sum += (pass[i] + '0');
-
-															putchar(pass[i] + '0');
-
-																	if ((2772 - sum) - '0' < 78)
-
-																				{
-
-																								n = 2772 - sum - '0';
-
-																											sum += n;
-
-																														putchar(n + '0');
-
-																																	break;
-
-																																			}
-
-																		}
-
-
-
-							return (0);
-
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }
-
-
