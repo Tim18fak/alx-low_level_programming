35612abd2b4a159d2843d0b15bc50938ff164801
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-20 15:09:49 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,39 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+int i, d, n, len, f, digit
 
-	do {
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
 
-		if (*s == '-')
-			sign *= -1;
+while (s[len] != '\0')
+	len++;
 
-		else if (*s >= '0' && 8s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-	} while (*s++);
-
-	return (num * sign);
+while (i < len && f == 0)
+{
+	if (s[i] == '-')
+		++d;
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
+		if (d % 2)
+			digit = -digit;
+		n = n * 10 + digit;
+		f = 1;
+	if (s[i + 1] < '0' || s[i + 1] > '9')
+		break;
+	f = 0;
+	}
+	i++;
 }
 
+if (f == 0)
+	return (0);
+
+	return (n);
+}
 
