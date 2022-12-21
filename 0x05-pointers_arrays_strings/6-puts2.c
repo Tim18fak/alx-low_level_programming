349aedadb6e4a845d3ece4b0fdb2de87a72cd8a2
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-20 17:26:15 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,17 +7,23 @@
  */
 void puts2(char *str)
 {
-	int count = 0;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	while (count >= 0)
+	while (*y != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		if (count % 2 == 0)
-			_putchar(str[count]);
-		count++
+		y++;
+		longi++;
 	}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
 }
