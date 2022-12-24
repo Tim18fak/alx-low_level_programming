205a3c1eb7d05df17c37cb6a71a2f5993e1b9a8d
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-23 17:18:03 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,42 +10,42 @@
 
 void print_buffer(char *b, int size)
 {
-	int o, j, i
+	int byte, index
 
-	o = 0;
+		for (byte = 0; byte < size; byte += 10)
+		{
+			printf("%08x: ", byte);
 
+			for (index = 0; index < 10; index++)
+			{
+				if ((index + byte) >= size)
+
+					printf(" ")
+				else
+
+					printf("%02x", *(b + index + byte));
+
+				if ((index % 2) != 0 && index != 0)
+
+					printf(" ");
+			}
+			for (index = 0; index < 10; index++)
+			{
+				if ((index + byte) >= size)
+					break;
+
+				else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+					printf("%c", *(b + index + byte));
+
+				else
+					printf(" ");
+			}
+
+			if (byte >= size)
+				continue;
+
+			printf("\n");
+		}
 	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x:", o);
-		for (i = 0; i < 10; i++)
-		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf(" ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
-
-			if (C < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
-		printf("\n");
-		o += 10;
-	}
+		print("\n");
 }
-
