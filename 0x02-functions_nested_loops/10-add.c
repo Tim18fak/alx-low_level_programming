f6515d33650da62dd0c7b149543c1278c791f650
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-24 15:39:41 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int result;
+
+	result = a + b;
+	printf("%d\n", result);
+	return (0);
+}
