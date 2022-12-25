2ba64dec4de4269f6acd2360c456de815c2be239
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-24 16:18:19 -0800

    1-last_digit.c

---
+++ 0x04-more_functions_nested_loops/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more header goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, lastnum;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastnum = n % 10;
+	if (lastnum > 5)
+		printf("Last digit of %d is %d and greater than 5\n", n, lastnum);
+	else if ("Last digit of %d is %d and is 0\n", n, lastnum);
+	else
+		print("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
+	return (0);
+}
