f94a5c42991857908427fae2672241f51c593f52
Tim18fak <timothy.avell.olatunde@gmail.com>
2022-12-27 16:48:05 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ *  main -Entry point
  *
- * Return: Alway 0 (Success)
+ *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
-	return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
