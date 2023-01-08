1a20783f0e88180154622a8cf505c9fe234d5a19
Tim18fak <timothy.avell.olatunde@gmail.com>
2023-01-08 15:57:05 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - multiple numbers
+ * a: any number
+ * b: any other number
+ * c: result
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c = a * b;
+
+	printf("the result of the two number is %d\n", c);
+
+	return (0);
+}
